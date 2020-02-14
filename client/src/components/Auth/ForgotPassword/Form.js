353dfdcd8f5ddf6835afe5d4aecf6d2561b0ef08
Bon Mercado <bonhokage06@gmail.com>
2020-02-14 16:32:09 +0800

    add: styling to forgot password form

--- client/src/components/Auth/ForgotPassword/Form.js
+++ client/src/components/Auth/ForgotPassword/Form.js
@@ -2,33 +2,78 @@ import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import styled, { ThemeContext } from "styled-components";
 import ErrorMessage from "../../../helpers/ErrorMessage";
+const ForgotPasswordFormStyled = styled.form`
+  input:required {
+    box-shadow: none;
+  }
+  input:invalid {
+    box-shadow: none;
+  }
+  input {
+    background: none;
+    width: 100%;
+    color: black;
+    font-size: ${({ theme }) => theme.input.fontSize};
+    display: block;
+    border: none;
+    border-radius: 1;
+    border: none;
+    outline: 0;
+    border-bottom: 2px solid lightgrey;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    outline: 0;
+  }
+  input:focus {
+    border-color: ${({ theme }) => theme.input.focus.border.color};
+    transition: 3s;
+  }
+  button {
+    color: ${({ theme }) => theme.button.textColor.primary};
+    font-size: ${({ theme }) => theme.button.fontSize} !important;
+    background-color: lightgrey;
+    border: none;
+    box-shadow: 0px 3px 6px #908e8e;
+  }
+  button[type="submit"] {
+    background-color: ${({ theme }) => theme.button.backgroundColor.primary};
+    padding: 10px;
+    display: block;
+    margin: 10px auto;
+    border: none;
+    width: 100%;
+  }
+`;
 export default function Form({ userDetails, onSubmit, handleInputChange }) {
   const { register, handleSubmit, errors } = useForm();
   return (
-    <form
+    <ForgotPasswordFormStyled
       data-testid="app-forgot-password-form"
       method="POST"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <input
-        data-testid="app-forgot-password-input-email"
-        type="email"
-        name="email"
-        value={userDetails.email}
-        placeholder="Email"
-        onChange={({ target }) => {
-          handleInputChange("email", target.value);
-        }}
-        ref={register({ required: true })}
-      />
+      <div>
+        <input
+          data-testid="app-forgot-password-input-email"
+          type="email"
+          name="email"
+          value={userDetails.email}
+          placeholder="Email"
+          onChange={({ target }) => {
+            handleInputChange("email", target.value);
+          }}
+          ref={register({ required: true })}
+        />
+      </div>
+
       <ErrorMessage
         field={errors.email}
         errorType="required"
         message="Email is required."
       />
-      <button data-testid="app-forgot-password-send-button" type="button">
+      <button data-testid="app-forgot-password-send-button" type="submit">
         Send
       </button>
-    </form>
+    </ForgotPasswordFormStyled>
   );
 }
--- client/src/components/Auth/ForgotPassword/index.js
+++ client/src/components/Auth/ForgotPassword/index.js
@@ -1,5 +1,31 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Form from "./Form";
+const ForgotPasswordStyled = styled.div`
+  display: block;
+  margin: 0 auto;
+  background-color: white;
+  width: 40%;
+  overflow: auto;
+  height: auto;
+  padding: 20px;
+  position: relative;
+  top: 5vh;
+  box-shadow: 0 0 25px #eae9e9;
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+  h1 {
+    text-align: center;
+    font-weight: normal;
+  }
+  @media (min-width: 600px) {
+    h1 {
+      font-weight: bold;
+    }
+  }
+`;
 export default function index() {
   const [userDetails, setUserDetails] = useState({
     email: ""
@@ -8,15 +34,17 @@ export default function index() {
     setUserDetails({ ...userDetails, [id]: value });
   };
   const handleFormSubmit = values => {
+    console.log(values);
     //future backend code
   };
   return (
-    <div data-testid="app-forgot-password">
+    <ForgotPasswordStyled data-testid="app-forgot-password">
+      <h1 data-testid="app-forgot-password-header">Forgot Password</h1>
       <Form
         userDetails={userDetails}
         onSubmit={handleFormSubmit}
         handleInputChange={handleInputChange}
       />
-    </div>
+    </ForgotPasswordStyled>
   );
 }
