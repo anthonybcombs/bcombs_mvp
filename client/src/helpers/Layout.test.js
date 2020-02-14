4bfb5126e22b3522bfab24ebd8fdd93a3cfbc676
Bon Mercado <bonhokage06@gmail.com>
2020-02-14 21:53:34 +0800

    test: add more test in layout

--- client/src/helpers/Layout.test.js
+++ client/src/helpers/Layout.test.js
@@ -1,38 +1,41 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { createHistory, createMemorySource } from "@reach/router";
 import CreateTestComponent from "./CreateTestComponent";
 import Layout from "./Layout";
 afterEach(cleanup);
 describe("renders Layout", () => {
-  const component = (
-    <CreateTestComponent>
-      <Layout />
-    </CreateTestComponent>
-  );
+  const component = (history = "/") => {
+    return (
+      <CreateTestComponent history={history}>
+        <Layout />
+      </CreateTestComponent>
+    );
+  };
   test("renders without issue", () => {
-    const { getByTestId } = render(component);
+    const { getByTestId } = render(component());
     expect(getByTestId("app-layout")).toBeInTheDocument();
   });
   test("does contains bcombs logo in header", () => {
-    const { getByTestId } = render(component);
+    const { getByTestId } = render(component());
     expect(getByTestId("app-logo")).toBeInTheDocument();
     expect(getByTestId("app-logo").alt).toBe("Bcombs Logo");
   });
-  test("contains login button in header", () => {
-    const { getByTestId } = render(component);
+  test("contains login button in header if location is auth/forgot-password", () => {
+    const { getByTestId } = render(component("/auth/forgot-password"));
     const button = getByTestId("app-header-login");
     expect(button).toBeInTheDocument();
     expect(button.textContent).toBe("Login");
   });
-  test("contains register link in header", () => {
-    const { getByTestId } = render(component);
+  test("contains register link in header if location is auth/forgot-password", () => {
+    const { getByTestId } = render(component("/auth/forgot-password"));
     const button = getByTestId("app-header-register");
     expect(button).toBeInTheDocument();
     expect(button.textContent).toBe("Register");
   });
   test("contains footer", () => {
-    const { getByTestId } = render(component);
+    const { getByTestId, rerender } = render(component());
     expect(getByTestId("app-footer")).toBeInTheDocument();
   });
 });
