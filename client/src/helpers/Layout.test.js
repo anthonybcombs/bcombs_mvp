771a5fcc2c1395f5f4f7b9cd9a0ce7172bdb2f4f
Bon Mercado <bonhokage06@gmail.com>
2020-02-14 19:54:46 +0800

    test: test login and register button in default layout

--- client/src/helpers/Layout.test.js
+++ client/src/helpers/Layout.test.js
@@ -14,10 +14,22 @@ describe("renders Layout", () => {
     const { getByTestId } = render(component);
     expect(getByTestId("app-layout")).toBeInTheDocument();
   });
-  test("does contains bcombs logo", () => {
+  test("does contains bcombs logo in header", () => {
     const { getByTestId } = render(component);
     expect(getByTestId("app-logo")).toBeInTheDocument();
     expect(getByTestId("app-logo").alt).toBe("Bcombs Logo");
+  });
+  test("contains login button in header", () => {
+    const { getByTestId } = render(component);
+    const button = getByTestId("app-header-login");
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toBe("Login");
+  });
+  test("contains register link in header", () => {
+    const { getByTestId } = render(component);
+    const button = getByTestId("app-header-register");
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toBe("Register");
   });
   test("contains footer", () => {
     const { getByTestId } = render(component);
