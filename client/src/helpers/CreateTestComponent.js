160db736a0b5c818dbf9b1a87f0ede699be6e2b7
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 16:30:26 +0800

    test: update createTestComponent to support routing test

--- client/src/helpers/CreateTestComponent.js
+++ client/src/helpers/CreateTestComponent.js
@@ -1,10 +1,21 @@
 import React, { StrictMode } from "react";
 import { ThemeContext } from "styled-components";
+import { Provider } from "react-redux";
+import {
+  LocationProvider,
+  createHistory,
+  createMemorySource
+} from "@reach/router";
+import { store } from "../redux/saga";
 import { defaultTheme } from "./Theme";
-export default function({ children }) {
+export default function({ children, history = "/" }) {
   return (
-    <ThemeContext.Provider value={defaultTheme}>
-      <StrictMode>{children}</StrictMode>
-    </ThemeContext.Provider>
+    <Provider store={store}>
+      <LocationProvider history={createHistory(createMemorySource(history))}>
+        <ThemeContext.Provider value={defaultTheme}>
+          <StrictMode>{children}</StrictMode>
+        </ThemeContext.Provider>
+      </LocationProvider>
+    </Provider>
   );
 }
