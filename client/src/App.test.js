a7229f21415d1834c64ef02990ad7818b299a510
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 16:26:08 +0800

    test: add tests for authenticated layout

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CreateTestComponent from "./helpers/CreateTestComponent";
 import App from "./App";
+import requestAuth from "./redux/actions/Auth";
 afterEach(cleanup);
 describe("renders App", () => {
   const component = (
@@ -10,8 +11,23 @@ describe("renders App", () => {
       <App />
     </CreateTestComponent>
   );
-  test("renders without issue", () => {
-    const { getByTestId } = render(component);
-    expect(getByTestId("app")).toBeInTheDocument();
+  describe("not authenticated", () => {
+    test("renders without issue", () => {
+      const { getByTestId } = render(component);
+      expect(getByTestId("app")).toBeInTheDocument();
+    });
+  });
+  describe("authenticated", () => {
+    // test("renders without issue", async () => {
+    //   const { getByTestId, rerender } = render(component);
+    //   const loginForm = getByTestId("app-login-form");
+    //   // await act(async () => {
+    //   //   fireEvent.submit(loginForm, {
+    //   //     email: "bon@yahoo.com",
+    //   //     password: "bonbon"
+    //   //   });
+    //   // });
+    //   expect(getByTestId("app-dashboard")).toBeInTheDocument();
+    // });
   });
 });
