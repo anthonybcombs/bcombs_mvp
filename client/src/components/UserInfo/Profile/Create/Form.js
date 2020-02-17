110db830604369e085d5a1988192b71bd53653d9
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 18:35:37 +0800

    fix: add new styles in create profile

--- client/src/components/UserInfo/Profile/Create/Form.js
+++ client/src/components/UserInfo/Profile/Create/Form.js
@@ -1,9 +1,72 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import styled, { ThemeContext } from "styled-components";
+const CreateProfileStyled = styled.form`
+  input:required {
+    box-shadow: none;
+  }
+  input:invalid {
+    box-shadow: none;
+  }
+  input,
+  select {
+    background: none;
+    width: 100%;
+    color: black;
+    font-size: ${({ theme }) => theme.input.fontSize};
+    display: block;
+    border: none;
+    border-radius: 1;
+    border: none;
+    outline: 0;
+    border-bottom: 2px solid lightgrey;
+    outline: 0;
+    margin-top: 2.5em;
+    margin-bottom: 2.5em;
+  }
+  input:focus {
+    border-color: ${({ theme }) => theme.input.focus.border.color};
+    transition: 3s;
+  }
+  button {
+    color: ${({ theme }) => theme.button.textColor.primary};
+    font-size: ${({ theme }) => theme.button.fontSize} !important;
+    background-color: lightgrey;
+    border: none;
+    box-shadow: 0px 3px 6px #908e8e;
+  }
+  button[type="submit"] {
+    background-color: ${({ theme }) => theme.button.backgroundColor.primary};
+    padding: 10px;
+    display: block;
+    margin: 10px auto;
+    border: none;
+    width: 200px !important;
+    margin-top: 0.5em;
+  }
+  select {
+    display: block;
+    width: 100%;
+    font-size: ${({ theme }) => theme.select.fontSize};
+    border: none;
+  }
+  select option {
+    font-weight: normal;
+  }
+  h3 {
+    text-align: center;
+  }
+  @media (min-width: 600px) {
+    #userTypes {
+      grid-template-columns: 50% 50%;
+      grid-gap: 0;
+    }
+  }
+`;
 export default function Form() {
+  const theme = useContext(ThemeContext);
   return (
-    <form>
-      <h2>Create my profile</h2>
+    <CreateProfileStyled method="POST" theme={theme}>
+      <h3>Create my profile</h3>
       <input
         data-testid="app-profile-create-input-firstname"
         name="firstname"
@@ -15,6 +78,17 @@ export default function Form() {
         placeholder="Last name"
       />
       <select
+        data-testid="app-profile-create-select-gender"
+        name="gender"
+        defaultValue={""}
+      >
+        <option value="" disabled>
+          Gender
+        </option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      <select
         data-testid="app-profile-create-select-family-relationship"
         name="family-relationship"
         defaultValue={""}
@@ -25,17 +99,6 @@ export default function Form() {
         <option value="father">Father</option>
         <option value="mother">Mother</option>
         <option value="sibling">Sibling</option>
-      </select>
-      <select
-        data-testid="app-profile-create-select-gender"
-        name="gender"
-        defaultValue={""}
-      >
-        <option value="" disabled>
-          Gender
-        </option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
       </select>
       <input
         data-testid="app-profile-create-input-zip-code"
@@ -52,6 +115,6 @@ export default function Form() {
       <button data-testid="app-profile-create-submit-button" type="submit">
         Save and Continue
       </button>
-    </form>
+    </CreateProfileStyled>
   );
 }
--- client/src/components/UserInfo/Profile/Create/index.js
+++ client/src/components/UserInfo/Profile/Create/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 import Form from "./Form";
+const CreateProfileStyled = styled.div`
+  display: block;
+  margin: 0 auto;
+  background-color: white;
+  width: 60%;
+  overflow: auto;
+  height: auto;
+  padding: 5px 30px 5px 30px;
+  position: relative;
+  top: 3vh;
+  box-shadow: 0 0 25px #eae9e9;
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+  h2 {
+    text-align: center;
+    font-weight: normal;
+  }
+  @media (min-width: 600px) {
+    h2 {
+      font-weight: bold;
+    }
+  }
+`;
 export default function index() {
   return (
-    <div data-testid="app-profile-create">
-      <h1>Let's get started</h1>
+    <CreateProfileStyled data-testid="app-profile-create">
+      <h2>Let's get started</h2>
       <Form />
-    </div>
+    </CreateProfileStyled>
   );
 }
