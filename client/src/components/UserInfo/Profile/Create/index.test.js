7348162eee5fe170be03113d08a20c2e8adc0683
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 20:13:04 +0800

    test: added more test in create profile

--- client/src/components/UserInfo/Profile/Create/index.test.js
+++ client/src/components/UserInfo/Profile/Create/index.test.js
@@ -95,17 +95,109 @@ describe("Create Profile", () => {
     });
   });
   describe("test behaivor", () => {
-    test("date type switching from text to date", () => {
-      const { getByTestId } = render(component);
-      const inputDateOfBirth = getByTestId(
-        "app-profile-create-input-date-of-birth"
-      );
-      expect(inputDateOfBirth).toBeInTheDocument();
-      fireEvent.focus(inputDateOfBirth);
-      expect(inputDateOfBirth.type).toBe("date");
-      fireEvent.blur(inputDateOfBirth);
-      expect(inputDateOfBirth.type).toBe("text");
-      expect(inputDateOfBirth.placeholder).toBe("Date of Birth");
+    describe("Input Firstname", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const inputFirstname = getByTestId(
+          "app-profile-create-input-firstname"
+        );
+        expect(inputFirstname.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const inputFirstname = getByTestId(
+          "app-profile-create-input-firstname"
+        );
+        fireEvent.change(inputFirstname, { target: { value: "bon nicolai" } });
+        expect(inputFirstname.value).toBe("bon nicolai");
+      });
+    });
+    describe("Input Lastname", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const inputlastName = getByTestId("app-profile-create-input-lastname");
+        expect(inputlastName.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const inputLastname = getByTestId("app-profile-create-input-lastname");
+        fireEvent.change(inputLastname, { target: { value: "mercado" } });
+        expect(inputLastname.value).toBe("mercado");
+      });
+    });
+    describe("Select Gender", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const selectGender = getByTestId("app-profile-create-select-gender");
+        expect(selectGender.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const selectGender = getByTestId("app-profile-create-select-gender");
+        fireEvent.change(selectGender, { target: { value: "male" } });
+        expect(selectGender.value).toBe("male");
+      });
+    });
+    describe("Select Family relationship", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const selectFamilyRelationship = getByTestId(
+          "app-profile-create-select-family-relationship"
+        );
+        expect(selectFamilyRelationship.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const selectFamilyRelationship = getByTestId(
+          "app-profile-create-select-family-relationship"
+        );
+        fireEvent.change(selectFamilyRelationship, {
+          target: { value: "father" }
+        });
+        expect(selectFamilyRelationship.value).toBe("father");
+      });
+    });
+    describe("input Zip code", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const inputZipCode = getByTestId("app-profile-create-input-zip-code");
+        expect(inputZipCode.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const inputZipCode = getByTestId("app-profile-create-input-zip-code");
+        fireEvent.change(inputZipCode, { target: { value: "2020" } });
+        expect(inputZipCode.value).toBe("2020");
+      });
+    });
+    describe("date of birth", () => {
+      test("test initial value", () => {
+        const { getByTestId } = render(component);
+        const inputdateofbirth = getByTestId(
+          "app-profile-create-input-date-of-birth"
+        );
+        expect(inputdateofbirth.value.length).toBe(0);
+      });
+      test("test value when changed", () => {
+        const { getByTestId } = render(component);
+        const inputdateofbirth = getByTestId(
+          "app-profile-create-input-date-of-birth"
+        );
+        fireEvent.change(inputdateofbirth, { target: { value: "01/29/2019" } });
+        expect(inputdateofbirth.value).toBe("01/29/2019");
+      });
+      test("date type switching from text to date", () => {
+        const { getByTestId } = render(component);
+        const inputDateOfBirth = getByTestId(
+          "app-profile-create-input-date-of-birth"
+        );
+        expect(inputDateOfBirth).toBeInTheDocument();
+        fireEvent.focus(inputDateOfBirth);
+        expect(inputDateOfBirth.type).toBe("date");
+        fireEvent.blur(inputDateOfBirth);
+        expect(inputDateOfBirth.type).toBe("text");
+        expect(inputDateOfBirth.placeholder).toBe("Date of Birth");
+      });
     });
   });
 });
