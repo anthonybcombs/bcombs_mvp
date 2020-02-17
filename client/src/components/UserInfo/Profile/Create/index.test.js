599625aba9bb8c34579e5bebbcc3328e8bd02b9e
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 17:24:50 +0800

    test: add tests for create profile

---
+++ client/src/components/UserInfo/Profile/Create/index.test.js
@@ -0,0 +1,30 @@
+import React, { StrictMode } from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import CreateTestComponent from "../../../../helpers/CreateTestComponent";
+import CreateProfile from ".";
+afterEach(cleanup);
+
+describe("Create Profile", () => {
+  const component = (
+    <CreateTestComponent>
+      <CreateProfile />
+    </CreateTestComponent>
+  );
+  describe("test renders", () => {
+    test("does contains header with text of Let's get started", () => {
+      const { getByText } = render(component);
+      const header = getByText(/Let's get started/);
+      expect(header).toBeInTheDocument();
+      expect(header.textContent).toContain("Let's get started");
+    });
+    test("does contains header with text of Create my profile", () => {});
+    test("does contains input(firsname)", () => {});
+    test("does contains input(lastname)", () => {});
+    test("does contains select(family realtionships)", () => {});
+    test("does contains select(gender)", () => {});
+    test("does contains input(zip code)", () => {});
+    test("does contains input(date of birth)", () => {});
+    test("does contains save to continue submit button", () => {});
+  });
+});
