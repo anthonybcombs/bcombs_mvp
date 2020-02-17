16c4fbaf014e4635e83bfc47a5682285d99ec578
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 16:26:33 +0800

    add: dashboard layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Router } from "@reach/router";
 import Layout from "./helpers/Layout";
+import DashboardLayout from "./helpers/DashboardLayout";
 import Auth from "./components/Auth";
+import DashBoard from "./components/Dashboard/";
 import Login from "./components/Auth/Login";
 import CreateUser from "./components/Auth/Create";
 import ForgotPassword from "./components/Auth//ForgotPassword";
 export default function App() {
+  const authenticated = useSelector(state => state.auth.status === "SIGNED_IN");
   return (
-    <Layout>
-      <div data-testid="app">
-        <Router>
-          <Auth path="/">
-            <Login path="/" />
-            <CreateUser path="auth/create" />
-            <ForgotPassword path="auth/forgot-password" />
-          </Auth>
-        </Router>
-      </div>
-    </Layout>
+    <>
+      {authenticated ? (
+        <DashboardLayout>
+          <div data-testid="app-dashboard">
+            <DashBoard path="/dashboard"></DashBoard>
+          </div>
+        </DashboardLayout>
+      ) : (
+        <Layout>
+          <div data-testid="app">
+            <Router>
+              <Auth path="/">
+                <Login path="/" />
+                <CreateUser path="auth/create" />
+                <ForgotPassword path="auth/forgot-password" />
+              </Auth>
+            </Router>
+          </div>
+        </Layout>
+      )}
+    </>
   );
 }
