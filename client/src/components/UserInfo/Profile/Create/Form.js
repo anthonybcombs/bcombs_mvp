d5aa9d80ef0c61997ac4dfe8ab8960b1956d8e4d
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 17:31:27 +0800

    fix: implement sub heading in create profile

---
+++ client/src/components/UserInfo/Profile/Create/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form>
+      <h2>Create my profile</h2>
+    </form>
+  );
+}
--- client/src/components/UserInfo/Profile/Create/index.js
+++ client/src/components/UserInfo/Profile/Create/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Form from "./Form";
 export default function index() {
   return (
     <div data-testid="app-profile-create">
       <h1>Let's get started</h1>
+      <Form />
     </div>
   );
 }
