3d9aa70fc0684a807b5859af1deb24151162fe50
Bon Mercado <bonhokage06@gmail.com>
2020-02-17 17:37:16 +0800

    fix: add input firstname in create profile

--- client/src/components/UserInfo/Profile/Create/Form.js
+++ client/src/components/UserInfo/Profile/Create/Form.js
@@ -4,6 +4,11 @@ export default function Form() {
   return (
     <form>
       <h2>Create my profile</h2>
+      <input
+        data-testid="app-profile-create-input-username"
+        name="firstname"
+        placeholder="First name"
+      />
     </form>
   );
 }
