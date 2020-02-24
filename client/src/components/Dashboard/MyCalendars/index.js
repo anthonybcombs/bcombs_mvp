47ad6ec96aa99dd1d492f02e8ab49d9642f1d8b6
Bon Mercado <bonhokage06@gmail.com>
2020-02-25 01:18:26 +0800

    fix: my calendar padding

--- client/src/components/Dashboard/MyCalendars/index.js
+++ client/src/components/Dashboard/MyCalendars/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import BigCalendar from "../../Calendar/big-calendar/";
+const MyCalendarStyled = styled.div`
+  margin: 1em;
+`;
 export default function index() {
   const { calendars, events } = useSelector(({ calendars, events }) => ({
     calendars,
     events
   }));
   return (
-    <div>
+    <MyCalendarStyled>
       <h1>My Calendars</h1>
       <BigCalendar events={events} calendars={calendars} />
-    </div>
+    </MyCalendarStyled>
   );
 }
