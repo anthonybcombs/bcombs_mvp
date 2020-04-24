ee78e040527e86da7c8afafbf4fd97a4188cbe16
Bon Mercado <bonhokage06@gmail.com>
2020-04-25 02:09:55 +0800

    add: express security

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 import "regenerator-runtime/runtime";
 require("dotenv").config();
 import apiRouter from "./api/";
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
