540a8ff33c9d97fe31be09ee7ef2448832e7adf6
Bon Mercado <bonhokage06@gmail.com>
2020-04-29 21:32:41 +0800

    fix: contacts

--- server/api/contacts/index.js
+++ server/api/contacts/index.js
@@ -16,12 +16,14 @@ export const getContacts = async () => {
   }
 };
 
-export const removeContact = async id => {
+export const removeContact = async (id) => {
   const db = makeDb();
   let result = [];
   try {
     await db.query(`DELETE FROM contacts WHERE id=UUID_TO_BIN(?)`, [id]);
-      await db.query(`DELETE FROM group_members WHERE user_id=UUID_TO_BIN(?)`, [id]);
+    await db.query(`DELETE FROM group_members WHERE user_id=UUID_TO_BIN(?)`, [
+      id,
+    ]);
     const rows = await db.query(
       `SELECT BIN_TO_UUID(id) as id,first_name,last_name,email,phone_number  from contacts`
     );
@@ -33,15 +35,16 @@ export const removeContact = async id => {
   }
 };
 
-export const editContact = async data => {
+export const editContact = async (data) => {
   const db = makeDb();
   let result = [];
   try {
-    const {first_name,last_name,phone_number,email,relation, id} = data;
-    await db.query(`UPDATE contacts SET first_name=?,last_name=?,phone_number=?,email=?,relation=? WHERE id=UUID_TO_BIN(?)`, [
-      first_name,last_name,phone_number,email,relation, id
-    ]);
-  
+    const { first_name, last_name, phone_number, email, relation, id } = data;
+    await db.query(
+      `UPDATE contacts SET first_name=?,last_name=?,phone_number=?,email=?,relation=? WHERE id=UUID_TO_BIN(?)`,
+      [first_name, last_name, phone_number, email, relation, id]
+    );
+
     const rows = await db.query(
       `SELECT BIN_TO_UUID(id) as id,first_name,last_name,email,phone_number  from contacts`
     );
