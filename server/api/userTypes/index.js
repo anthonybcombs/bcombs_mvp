80df6f21b3cde25f359d3f62dd1d4fb2fcdc1fea
Bon Mercado <bonhokage06@gmail.com>
2020-05-17 12:05:57 +0800

    enable caching on userTypes

--- server/api/userTypes/index.js
+++ server/api/userTypes/index.js
@@ -1,17 +1,26 @@
 import { makeDb } from "../../helpers/database";
-
+import client, { getRedisKey } from "../../services/redis";
 export const getUserTypes = async () => {
   const db = makeDb();
   const result = [];
   try {
-    const userTypes = await db.query(
-      "SELECT BIN_TO_UUID(ID) as id,name FROM user_types"
-    );
+    let userTypes;
+    const userTypesCache = JSON.parse(await getRedisKey("userTypes"));
+    if (userTypesCache === null) {
+      userTypes = await db.query(
+        "SELECT BIN_TO_UUID(ID) as id,name FROM user_types"
+      );
+      client.set("userTypes", JSON.stringify(userTypes));
+      client.EXPIRE(["userTypes", "5"]);
+    } else {
+      userTypes = userTypesCache;
+    }
     userTypes.forEach((userType) => {
       result.push({ id: userType.id, name: userType.name });
     });
     return result;
   } catch (error) {
+    console.log(error);
   } finally {
     await db.close();
   }
