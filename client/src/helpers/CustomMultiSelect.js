5a5012d35ef3e6c9b7fdc6a3b054d1a9408e53df
dennis <sanjosedennisns7593@gmail.com>
2020-05-19 20:09:33 +0800

    Multiselect: Custom component

---
+++ client/src/helpers/CustomMultiSelect.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { Multiselect } from "multiselect-react-dropdown";
+
+const CustomMultiSelectContainer = styled.div`
+  #multiselectContainerReact .optionContainer li:hover,
+  #multiselectContainerReact .optionContainer li.highlight {
+    background: #f26e21;
+  }
+
+  #multiselectContainerReact div:first-child {
+    border: 0;
+    border-bottom: 2px solid #ccc;
+    border-radius: 0;
+  }
+
+  #multiselectContainerReact .searchBox {
+    font-size: 18px;
+    padding: 5px 0;
+    margin: 0;
+    margin-top: 2px;
+  }
+
+  #multiselectContainerReact .searchBox::placeholder {
+    font-size: 12px;
+  }
+
+  #multiselectContainerReact .chip {
+    background: #f26e21;
+  }
+`;
+const CustomMultiSelect = ({
+  onSelect,
+  onRemove,
+  placeholder,
+  displayValue,
+  closeIcon = "cancel",
+  className = "field-input",
+  options = [],
+  hasSelectAll = false
+}) => {
+  return (
+    <CustomMultiSelectContainer>
+      <Multiselect
+        className={className}
+        options={options}
+        hasSelectAll={hasSelectAll}
+        onSelect={onSelect}
+        onRemove={onRemove}
+        placeholder={placeholder}
+        displayValue={displayValue}
+        closeIcon={closeIcon}
+      />
+    </CustomMultiSelectContainer>
+  );
+};
+
+export default CustomMultiSelect;
