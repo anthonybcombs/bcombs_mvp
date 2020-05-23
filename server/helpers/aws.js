259abcddc2157c3113af048aa4682d3a0495c5f1
dennis <sanjosedennisns7593@gmail.com>
2020-05-23 22:33:49 +0800

    Update aws credentials

--- server/helpers/aws.js
+++ server/helpers/aws.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 
-const currentS3BucketName = "bcombs-dev";
-const s3BucketRootPath = "https://bcombs-dev.s3.amazonaws.com/";
+const currentS3BucketName = "bcombs";
+const s3BucketRootPath = "https://bcombs.s3.amazonaws.com/";
 // const fileFilter = (req, file, cb) => {
 //   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
 //     cb(null, true);
@@ -11,14 +11,14 @@ const s3BucketRootPath = "https://bcombs-dev.s3.amazonaws.com/";
 // };
 
 const s3Bucket = new AWS.S3({
-  accessKeyId: "AKIAR3ORYVVFKTTPNTOF",
-  secretAccessKey: "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",
-  Bucket: "bcombs-dev",
-  region: "us-east-1",
+  accessKeyId: "AKIAUCPS6O5YQCI56DN7",
+  secretAccessKey: "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",
+  Bucket: "bcombs",
+  region: "us-east-1"
 });
-export const uploadFile = (data) => {
+export const uploadFile = data => {
   return new Promise((resolve, reject) => {
-    s3Bucket.putObject(data, function (err, data) {
+    s3Bucket.putObject(data, function(err, data) {
       if (err) {
         console.log("Error uploading data: ", data);
         reject("Error uploading data: ", data);
