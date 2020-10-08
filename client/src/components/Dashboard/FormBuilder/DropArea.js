79dda6ed068ba6f7ddf692eedfa46105773caa53
Jeff <jefferson.cogay@gmail.com>
2020-10-09 02:56:57 +0800

    Sortable field groups

--- client/src/components/Dashboard/FormBuilder/DropArea.js
+++ client/src/components/Dashboard/FormBuilder/DropArea.js
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { useDrop } from 'react-dnd'
+import { uuid } from 'uuidv4'
+import update from 'immutability-helper'
 
 import { Items } from './Constants'
-import FieldConstructor from './FieldConstructor'
+import SortableGroup from './SortableGroup'
 
 export default () => {
   const [droppedFields, setDrop] = useState([])
 
   const handleDrop = (field) => {
-    const newFields = [
-      ...droppedFields,
-      field
-    ]
-    setDrop(newFields)
+    const newFields = [...droppedFields]
+    if (!newFields.find(e => e.id === field.id)) {
+      newFields.push(field)
+      setDrop(newFields)
+    }
   }
 
-  const [{ item }, drop] = useDrop({
+  const handleMoveGroup = (dragIndex, hoverIndex, draggedGroup) => {
+    let newFields = [...droppedFields]
+    if (dragIndex === undefined) {
+      newFields.push({ ...draggedGroup })
+      dragIndex = newFields.length - 1
+    }
+    const dragGroup = newFields[dragIndex]
+    setDrop(update(newFields, {
+        $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragGroup],
+        ],
+    }))
+  }
+
+  const [{ item, didDrop }, drop] = useDrop({
     accept: Object.values(Items),
-    drop: () => handleDrop(item),
+    drop: () => handleDrop(item, didDrop),
     collect: monitor => ({
       isOver: !!monitor.isOver(),
       item: monitor.getItem()
@@ -28,21 +45,9 @@ export default () => {
     <div className='fb-drop-area' ref={drop}>
       Drop here
       {
-        droppedFields.map(({ fields, name }) => {
+        droppedFields.map((fieldProps, index) => {
           return (
-            <div>
-              <div>{name}</div>
-              {
-                fields.map(({ key, label, placeholder = '', type, tag }) => {
-                  return FieldConstructor[tag]({
-                    name: key,
-                    placeholder,
-                    type,
-                    label
-                  })
-                })
-              }
-            </div>
+            <SortableGroup key={fieldProps.id} {...fieldProps} index={index} onMoveGroup={handleMoveGroup} />
           )
         })
       }
--- client/src/components/Dashboard/FormBuilder/FieldConstructor.js
+++ client/src/components/Dashboard/FormBuilder/FieldConstructor.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export default {
   input: ({ label, ...rest }) => {
-    console.log('zzzz', rest)
     return (
       <input
         {...rest}
