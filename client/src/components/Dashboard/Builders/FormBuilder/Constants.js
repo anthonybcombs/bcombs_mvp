e36e3ea2ccfd0d1cee96a174af2d0039b1636544
Clark <clarky27quinatadcan@gmail.com>
2020-11-05 22:11:30 +0800

    update

--- client/src/components/Dashboard/Builders/FormBuilder/Constants.js
+++ client/src/components/Dashboard/Builders/FormBuilder/Constants.js
@@ -226,6 +226,7 @@ export const StandardFields = [
     displayLabel: 'Checkbox Grid',
     label: 'Checkbox grid question',
     type: 'checkboxGrid',
+    gridMax: 1,
     isQa: true,
     includeLogic: false,
   },
