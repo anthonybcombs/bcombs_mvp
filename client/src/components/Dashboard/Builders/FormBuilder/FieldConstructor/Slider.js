e03cd08a26be101c9f9bd14a74fc2c80ba9026f0
Jeff <jefferson.cogay@gmail.com>
2020-11-06 02:42:29 +0800

    Slider

--- client/src/components/Dashboard/Builders/FormBuilder/FieldConstructor/Slider.js
+++ client/src/components/Dashboard/Builders/FormBuilder/FieldConstructor/Slider.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 
-export default ({ scale, scaleLabels, onChangeFieldSettings }) => {
+export default ({ scale, scaleLabels, onChangeFieldSettings, isBuilder, id: fieldId, onChange, value }) => {
   const handleChangeValues = ({ target: { value } }, type, subType) => {
     const currentTypeData = type === 'scale' ? { ...scale } : { ...scaleLabels }
     onChangeFieldSettings({ [type]: { ...currentTypeData, [subType]: value } })
-    console.log('atay', { value, a: value < 0, b: typeof value, c: isNaN(value)})
   }
 
   const handleBlurValues = (type) => {
@@ -21,68 +20,94 @@ export default ({ scale, scaleLabels, onChangeFieldSettings }) => {
     }
     onChangeFieldSettings({ scale: { min, max }})
   }
+
+  const handleAnswer = ({ target: { value: sliderValue } }) => {
+    onChange({ target: { id: fieldId, value: sliderValue } })
+  }
+
   return (
-    // <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-    <div className='slider-container'>
-      <div className='tooltip-wrapper'>
-        <p className='label'>Scale Range
-          <FontAwesomeIcon className='exclude-global' icon={faQuestionCircle}/>
-        </p>
-        <span className='tooltip'>Scale range must be a whole number from 0 or greater.</span>
-      </div>
-      <div className='slider-scale-number'>
-        <input
-          type='number'
-          value={scale.min}
-          className='field-input'
-          onChange={(e) => handleChangeValues(e, 'scale', 'min')}
-          onBlur={(e) => handleBlurValues('min')}
-        />
-        <span>to</span>
-        <input
-          type='number'
-          value={scale.max}
-          className='field-input'
-          onChange={(e) => handleChangeValues(e, 'scale', 'max')}
-          onBlur={(e) => handleBlurValues('max')}
-        />
-      </div>
+    <>
+      {
+        isBuilder ? (
+          <div className='slider-container'>
+            <div className='tooltip-wrapper'>
+              <p className='label'>Scale Range
+                <FontAwesomeIcon className='exclude-global' icon={faQuestionCircle}/>
+              </p>
+              <span className='tooltip'>Scale range must be a whole number from 0 or greater.</span>
+            </div>
+            <div className='slider-scale-number'>
+              <input
+                type='number'
+                value={scale.min}
+                className='field-input'
+                onChange={(e) => handleChangeValues(e, 'scale', 'min')}
+                onBlur={(e) => handleBlurValues('min')}
+              />
+              <span>to</span>
+              <input
+                type='number'
+                value={scale.max}
+                className='field-input'
+                onChange={(e) => handleChangeValues(e, 'scale', 'max')}
+                onBlur={(e) => handleBlurValues('max')}
+              />
+            </div>
 
-      <div className='slider-scale-range'>
-        <p>Scale Range Labels</p>
-        <label>
-          <span>Left Label</span>
-          <input
-            type='text'
-            placeholder='Label'
-            className='field-input'
-            value={scaleLabels.left}
-            onChange={(e) => handleChangeValues(e, 'scaleLabels', 'left')}
-          />
-        </label>
+            <div className='slider-scale-range'>
+              <p>Scale Range Labels</p>
+              <label>
+                <span>Left Label</span>
+                <input
+                  type='text'
+                  placeholder='Label'
+                  className='field-input'
+                  value={scaleLabels.left}
+                  onChange={(e) => handleChangeValues(e, 'scaleLabels', 'left')}
+                />
+              </label>
 
-        <label>
-          <span>Center Label</span>
-          <input
-            type='text'
-            className='field-input'
-            placeholder='Label (optional)'
-            value={scaleLabels.center}
-            onChange={(e) => handleChangeValues(e, 'scaleLabels', 'center')}
-          />
-        </label>
+              <label>
+                <span>Center Label</span>
+                <input
+                  type='text'
+                  className='field-input'
+                  placeholder='Label (optional)'
+                  value={scaleLabels.center}
+                  onChange={(e) => handleChangeValues(e, 'scaleLabels', 'center')}
+                />
+              </label>
 
-        <label>
-          <span>Right Label</span>
-          <input
-            type='text'
-            placeholder='Label'
-            className='field-input'
-            value={scaleLabels.right}
-            onChange={(e) => handleChangeValues(e, 'scaleLabels', 'right')}
-          />
-        </label>
-      </div>
-    </div>
+              <label>
+                <span>Right Label</span>
+                <input
+                  type='text'
+                  placeholder='Label'
+                  className='field-input'
+                  value={scaleLabels.right}
+                  onChange={(e) => handleChangeValues(e, 'scaleLabels', 'right')}
+                />
+              </label>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div>{scaleLabels.left}</div>
+              <div>{scaleLabels.center}</div>
+              <div>{scaleLabels.right}</div>
+            </div>
+            <input
+              style={{ width: '100%' }}
+              type='range'
+              min={scale.min}
+              max={scale.max}
+              value={value}
+              onChange={(e) => handleAnswer(e)}
+            />
+          </div>
+        )
+      }
+    </>
   )
 }
