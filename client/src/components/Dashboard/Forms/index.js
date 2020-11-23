970b037250f0a32938a6048924c33082ff0edeee
Clark <clarky27quinatadcan@gmail.com>
2020-11-24 01:37:53 +0800

    form manager

--- client/src/components/Dashboard/Forms/index.js
+++ client/src/components/Dashboard/Forms/index.js
@@ -87,6 +87,7 @@ export default (props) => {
 
   return (
     <FormStyled>
+      <h2>Form Manager</h2>
       <Headers />
       {
         isLoading ? (
--- client/src/components/Dashboard/Forms/styles.js
+++ client/src/components/Dashboard/Forms/styles.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
 export default styled.div`
+  width: auto;
+  margin: auto;
+  max-width: 1920px;
+  padding: 0rem 3em 2rem;
+
+  
 `;
