b5c48d319915375dbd13504973d78db28919619a
Jerome Yvan Apolonio <jerome.apolonio.ns@gmail.com>
2021-02-10 15:28:06 +0800

    Update csv export

--- client/src/helpers/ExportFilter.js
+++ client/src/helpers/ExportFilter.js
@@ -6,6 +6,8 @@ import { format } from "date-fns";
 
 import { Multiselect } from "multiselect-react-dropdown";
 
+import { exportHeaders } from "./ExportHeaders";
+
 const ExportFilterModal = styled.div`
   .modal {
     padding: 0;
@@ -100,7 +102,8 @@ const ExportFilter = ({
   filename = ""
 }) => {
 
-  console.log("filename filename", filename);
+  console.log("vendor", vendor);
+
   const CLASS_OPTIONS = [
     "Seniors",
     "Juniors",
@@ -117,6 +120,12 @@ const ExportFilter = ({
     { name: "Waiting List", value: "waiting_list" },
     { name: "No longer a Student", value: "no_longer_student" },
     { name: "Missed opportunity", value: "missed_opportunity" }
+  ];
+
+  const VERIFICATION_OPTIONS = [
+    { name: "Waiting for Verification", value: "waiting_for_verification"},
+    { name: "Verified", value: "verified" },
+    { name: "Rejected", value: "rejected"}
   ];
 
   const COLOR_OPTIONS = ["Blue", "Red", "Green"];
@@ -200,9 +209,8 @@ const ExportFilter = ({
     );
   });
 
-  const getApplicationStatus = (student_status, verification) => {
+  const getApplicationStatus = (student_status) => {
     let studentStatusVal = "";
-    let verificationVal = "";
 
     if (student_status == "new_applicant_in_process") {
       studentStatusVal = "In process";
@@ -222,6 +230,31 @@ const ExportFilter = ({
 
     return studentStatusVal;
   };
+
+  const getVerificationStatus = (verification) => {
+    let verificationVal = ""
+    VERIFICATION_OPTIONS.map((item) => {
+      if(item.value == verification) {
+        verificationVal = item.name;
+        return;
+      }
+    })
+
+    return verificationVal;
+  }
+
+  const getClassTeacher = (id) => {
+    let classTeacher = "";
+
+    appGroups.map((ap) => {
+      if(ap.app_grp_id == id) {
+        classTeacher = ap.name;
+        return;
+      }
+    });
+
+    return classTeacher;
+  }
 
   const getPrimaryParentName = parents => {
     if (parents && parents.length > 0) {
@@ -276,7 +309,6 @@ const ExportFilter = ({
 
   //   exportApplications.push(tempApplication);
   // }
-
 
   let exportFilename;
 
@@ -335,27 +367,114 @@ const ExportFilter = ({
       for(const key1 of Object.keys(application)) {
         console.log("obj", key1);
 
-        if(key1 == "vendorPrograms" || key1 == "vendorLocationSites") {continue;};
+        if(key1 == "vendorPrograms" || 
+          key1 == "vendorLocationSites" || 
+          key1 == "app_id" || 
+          key1 == "is_daycare") {continue;};
 
         if(typeof application[key1] === 'object' && application[key1] !== null && typeof application[key1] !== 'undefined') {
           if(Array.isArray(application[key1])) {
+            // parent
             console.log("this is an array", application[key1]);
 
             const level1Arr = application[key1];
 
             for(const [i, arrObj] of level1Arr.entries()) {
+              delete arrObj.password;
+              delete arrObj.emergency_contacts;
+              delete arrObj.parent_id;
+
               for(const key3 of Object.keys(arrObj)) {
-                if(key3 != "password") {
-                  formattedApplication = {...formattedApplication, [key1+ "" + (i+1) +" "+key3]: arrObj[key3] ? arrObj[key3]: ""}
+                if(key3 != "password" || key3 !== 'parent_id') {
+                  if(key3 == 'birthdate') {
+                    const newDate = arrObj[key3] ? format(new Date(arrObj[key3]), DATE_FORMAT) : "";
+                    formattedApplication = {...formattedApplication, ["(Parent " + (i+1) +") "+ exportHeaders.parent[key3]]: newDate}
+                  } else {
+                    formattedApplication = {...formattedApplication, ["(Parent " + (i+1) +") "+ exportHeaders.parent[key3]]: arrObj[key3] ? arrObj[key3]: ""}
+                  }
+                  
                 }
               }
             }
 
           } else {
+            // child 
             console.log("this is an object", application[key1]);
-            const level1 = application[key1];
+            let level1 = application[key1];
+
+            if(level1.is_daycare) {
+              delete level1.phone_type;
+              delete level1.phone_number;
+              delete level1.phone_type2;
+              delete level1.phone_number2;
+              delete level1.email_type;
+              delete level1.email_address;
+              delete level1.email_type2;
+              delete level1.email_address2;
+              delete level1.gpa_cumulative_q1;
+              delete level1.gpa_cumulative_q2;
+              delete level1.gpa_cumulative_q3;
+              delete level1.gpa_cumulative_q4;
+              delete level1.gpa_quarter_q1;
+              delete level1.gpa_quarter_q2;
+              delete level1.gpa_quarter_q3;
+              delete level1.gpa_quarter_q4;
+              delete level1.grade_desc;
+              delete level1.grade_number;
+              delete level1.hobbies;
+              delete level1.school_name;
+              delete level1.school_phone;
+              delete level1.year_taken;
+              delete level1.life_events;
+              delete level1.career_goals;
+              delete level1.colleges;
+              delete level1.affiliations;
+              delete level1.awards;
+              delete level1.accomplishments;
+              delete level1.mentee_gain_program;
+              delete level1.class_rank;
+            } else {
+              delete level1.child_currently_doctors_care;
+              delete level1.comments_suggestion;
+              delete level1.does_child_require_physical_education_service;
+              delete level1.history_prev_diseases;
+              delete level1.is_child_transferring;
+              delete level1.list_any_allergies;
+              delete level1.list_fears_unique_behavior;
+              delete level1.list_special_dietary;
+              delete level1.medical_action_plan;
+              delete level1.mental_physical_disabilities;
+              delete level1.needed_days;
+              delete level1.preffered_start_date;
+              delete level1.primary_language;
+              delete level1.prev_school_address;
+              delete level1.prev_school_attended;
+              delete level1.prev_school_city;
+              delete level1.prev_school_phone;
+              delete level1.prev_school_state;
+              delete level1.prev_school_zip_code;
+              delete level1.schedule_tour;
+              delete level1.transfer_reason;
+              delete level1.voucher;
+              delete level1.reasons_previous_hospitalizations;
+              delete level1.current_classroom;
+
+              console.log("level1", level1);
+              console.log("exportHeaders.child.main['birthdate']", exportHeaders);
+            }
             for(const key2 of Object.keys(level1)) {
-              formattedApplication = {...formattedApplication, [key1+ " "+ key2]: level1[key2] ? level1[key2] : ""}
+              if(key2 == 'ch_id') { continue; }
+              console.log("key2", key2);
+              console.log("[exportHeaders.child.main[key2]]", [exportHeaders.child.main[key2]]);
+              if(key2 == 'birthdate') {
+                const newDate = level1[key2] ? format(new Date(level1[key2]), DATE_FORMAT) : "";
+                formattedApplication = {...formattedApplication, [exportHeaders.child.main['birthdate']]: newDate }
+              } else if(key2 == 'has_suspended') {
+                const val = level1[key2] == 1 ? 'Yes' : level1[key2] == 0 ? 'No' : '';
+                formattedApplication = {...formattedApplication, ['(Child) ' + exportHeaders.child.main['has_suspended']]: val}
+              } else {
+                formattedApplication = {...formattedApplication, ['(Child) ' + exportHeaders.child.main[key2]]: level1[key2] ? level1[key2] : ""}
+              }
             }
           }
         } else {
@@ -368,7 +487,7 @@ const ExportFilter = ({
               if(Array.isArray(ecs)) {
                 for(const [i, ec] of ecs.entries()) {
                   for(const key4 of Object.keys(ec)) {
-                    formattedApplication = {...formattedApplication, [key1 + "" + (i+1) + " " + key4]: ec[key4] ? ec[key4] : "" }
+                    formattedApplication = {...formattedApplication, ['Emergency Contacts' + "" + (i+1) + " " + exportHeaders.emergency_contacts[key4]]: ec[key4] ? ec[key4] : "" }
                   }
                 }
               } else {
@@ -378,11 +497,32 @@ const ExportFilter = ({
               formattedApplication = {...formattedApplication, [key1]: ""}
             }
           } else {
-            formattedApplication = {...formattedApplication, [key1]: application[key1] ? application[key1]: ""}
+            if(key1 == 'vendor') {
+              formattedApplication = {...formattedApplication, [exportHeaders['vendor']]: vendor?.name ? vendor.name: ""}
+            } else if(key1 == 'application_date' || 
+              key1 == 'section1_date_signed' || 
+              key1 == 'section2_date_signed' || 
+              key1 == 'section3_date_signed' ) {
+                const newDate = application[key1] ? format(new Date(application[key1]), DATE_FORMAT) : "";
+                formattedApplication = {...formattedApplication, [exportHeaders[key1]]: newDate}
+            } else {
+              let val;
+              if(key1 == 'student_status') {
+                val = getApplicationStatus(application[key1]);
+              } else if(key1 == 'verification') { 
+                val = getVerificationStatus(application[key1]);
+              } else if(key1 == 'class_teacher') {
+                val = getClassTeacher(application[key1]);
+              } else {
+                val = application[key1];
+              }
+              formattedApplication = {...formattedApplication, [exportHeaders[key1]]: val ? val : ""}
+            }
+            
           }
         }
       }
-
+      delete formattedApplication.undefined;
       console.log("formattedApplication", formattedApplication);
 
       exportApplications.push(formattedApplication);
@@ -390,7 +530,7 @@ const ExportFilter = ({
   }
 
   console.log("filterApplications", filterApplications);
-  //console.log("exportApplications", exportApplications);
+  console.log("exportApplications", exportApplications);
 
   const PROGRAMS_OPTIONS =
     app_programs.length > 0
