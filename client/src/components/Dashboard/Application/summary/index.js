8ac4b1265225b6958dd03d5afe49bdd04123eeb4
Jerome Yvan Apolonio <jerome.apolonio.ns@gmail.com>
2021-03-25 05:19:47 +0800

    Remove target on class link

--- client/src/components/Dashboard/Application/summary/index.js
+++ client/src/components/Dashboard/Application/summary/index.js
@@ -106,7 +106,7 @@ export default function index({
         return (
           <tr key={group.id}>
             <td>
-              <a href={classLink} target="_blank">
+              <a href={classLink}>
                 {group.name}
               </a>
             </td>
