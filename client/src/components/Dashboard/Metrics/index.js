170fcc5f47f5a987e01d4317aacb786439119969
kromley <kromley@gmail.com>
2021-07-20 11:42:50 -0400

    getting metrics frame started

---
+++ client/src/components/Dashboard/Metrics/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Metrics = props => {
+// const sql_stat = "Select xyz form a, b";
+// rows = sql_run(sql_stat)
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default Metrics;
