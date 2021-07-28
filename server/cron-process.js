4cd1a7e15bc7ea30ba9fde24683cd18f91c4b07f
Jerome Yvan Apolonio <jerome.apolonio.ns@gmail.com>
2021-07-28 20:28:32 +0800

    Update cron process

--- server/cron-process.js
+++ server/cron-process.js
@@ -174,7 +174,8 @@ exports.setReminder = () => {
 
           const col = {
             application: appl.app_id,
-            received_reminder: 1
+            received_reminder: 1,
+            received_update: 1
           };
 
           console.log('updateApplicationUser', col);
