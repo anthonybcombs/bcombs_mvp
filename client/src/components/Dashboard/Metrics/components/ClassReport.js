bfc2e55523a427c01bfb8187d76cbd8f923da477
kromley <kromley@gmail.com>
2021-08-31 13:25:03 -0400

    class engagement metric updated for form selection

--- client/src/components/Dashboard/Metrics/components/ClassReport.js
+++ client/src/components/Dashboard/Metrics/components/ClassReport.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState, useRef } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 
 import Charts from './Charts';
+import Loading from "../../../../helpers/Loading.js";
 
-const apiCallClassReportData = async (id, year, vendorId) => {
+const apiCallClassReportData = async (id, year, vendorId, formId) => {
     
     // Default options are marked with *
     const response = await fetch(`${process.env.API_HOST}/api/metrics/classes`, {
@@ -20,7 +21,8 @@ const apiCallClassReportData = async (id, year, vendorId) => {
         body: JSON.stringify({
             'id': id,
             'year': year,
-            'vendorId': vendorId
+            'vendorId': vendorId,
+            'formId' : formId
         }) // body data type must match "Content-Type" header
     });
     return response.json(); // parses JSON response into native JavaScript objects
@@ -31,21 +33,31 @@ const apiCallClassReportData = async (id, year, vendorId) => {
 
 const ClassReports = props => {
     const { auth, vendors } = props;
-
+    const [isLoading, setIsLoading] = useState(true);
+    const [formList, setFormList] = useState([]);
     const [tempOptionsData, setTempOptionsData] = useState([]);
     const [year, setYear] = useState('2021');
+    const [formIdLocal, setFormIdLocal] = useState('fid_0');
     const [hasData, setHasData] = useState(false);
     const chart = useRef();
 
-    const triggerApi2 = async (id, year, vendorsIn) => {
+    const triggerApi2 = async (id, year, vendorsIn, formId) => {
         try {
             if (!vendorsIn ||!vendorsIn.length) {
+                setIsLoading(false);
                 defineChart(null);
                 return;
             }
-            const vendorId = vendorsIn[0].id2; //TODO: add support for user in multiple vendors
-            const res = await apiCallClassReportData(id, year, vendorId);
+            setIsLoading(true);
+            const vendorId = vendorsIn[0].id2; 
+            const res = await apiCallClassReportData(id, year, vendorId, formId);
             console.log('apiCall classes ', res)
+            if (!res.formArray) {
+                setFormList([]);
+            }
+            else {
+                setFormList(res.formArray);
+            }
             if (res && res.classData) {
                 //setCurrentUser(res.user);
                 defineChart(res.classData);
@@ -53,6 +65,7 @@ const ClassReports = props => {
             else {
                 defineChart(null);
             }
+            setIsLoading(false);
         } catch (err) {
             console.log('Error', err)
         }
@@ -123,18 +136,38 @@ const ClassReports = props => {
         setYear(event.target.value);
         console.log("event ", event);
         console.log("year3 ", event.target.value); // ;year);
-        triggerApi2(auth.user_id, event.target.value, vendors);
+        triggerApi2(auth.user_id, event.target.value, vendors, 'fid_0');
+        setFormIdLocal('fid_0');
     };
     //console.log('vendors',vendors)
- 
+    const formChange =(event) => {
+        let formIdIn = event.target.value;
+        setFormIdLocal(formIdIn);
+        console.log("=========== form_id: ", formIdIn);
+        triggerApi2(auth.user_id, year, vendors, formIdIn);
+    };
+
+
     return <div style={{ padding: 24 }}>
         <div className="grid grid-2b">
-        <div className="top-left"><h4>Class Student Counts</h4><select id="mentee-year" onChange={yearChange} value={year}>
+        <div className="top-left"><h4>Class Student Counts</h4>
+        <select id="mentee-year" onChange={yearChange} value={year}>
             <option name="2022">2022</option>
             <option name="2021" >2021</option>
             <option name="2020">2020</option>
             <option name="2019">2019</option>
-            </select></div>
+            </select>
+        { isLoading ? ( <></>) : (
+                        <>
+                    <select id="vendor-form" onChange={formChange} value={formIdLocal}>
+                        { formList && formList.length > 0 && formList.map((elem) => 
+                                <option value={elem.key} key={elem.key}>{elem.name}</option> 
+                            )
+                        }
+                        </select>
+                        </>
+                    )}
+                    </div>
         <div className="top-right"><button onClick={exportChart} id="exportButton"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" className="svg-inline--fa fa-download fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg><span>Export</span></button></div>
         </div>
         {hasData === true && <Charts customRef={chart} optionsData={tempOptionsData} />}
--- server/api/metrics/classes/index.js
+++ server/api/metrics/classes/index.js
@@ -1,28 +1,50 @@
 import express from "express";
 
 import { makeDb } from "../../../helpers/database";
+import { 
+    getFormsByVendorId, 
+} from "../form_and_class_queries";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
 
     try {
-        const { id, year, vendorId } = req.body;
+        const { id, year, vendorId, formId } = req.body;
         const db = makeDb();
         console.log('m ID', id);
         console.log('vendor ', vendorId );
+
+        let formArray = await getFormsByVendorId(db, vendorId);
+        if (!formArray.length) {
+            res.status(200).json({ classData: [], formArray: [] });
+            return;
+        }
+        console.log('getting class list');
+
+
         let dtLastTxt = '' + (year - 1) + '-08-01';
         let dtLast = new Date(dtLastTxt);
         let dtNextText = '' + year + '-08-01'; 
         let dtNext = new Date(dtNextText);
 
         let queryParam = [vendorId];
-
         let query = "SELECT count(a.child_id) as numStudents, b.name " + 
             "FROM vendor_app_groups_to_student a, vendor_app_groups b, vendor c " +
             "where a.app_grp_id = b.app_grp_id " + 
             "and c.id2 = ? and b.vendor = c.id " +
             "group by b.app_grp_id";
+
+
+        if (formId && formId != 'fid_0') {
+            query = "SELECT count(a.child_id) as numStudents, b.name " + 
+                "FROM vendor_app_groups_to_student a, vendor_app_groups b " +
+                "where b.form = UUID_TO_BIN(?) " + 
+                "and a.app_grp_id = b.app_grp_id " +
+                "group by b.app_grp_id";
+            queryParam = [formId];
+        }
+
         console.log('Query ', query);
         const response =  await db.query(query, queryParam);
         console.log('student counts ', response);
@@ -33,9 +55,9 @@ router.post("/", async (req, res) => {
             returnData.push({ name: row.name, y: row.numStudents });
         }
 
-        res.status(200).json({ classData: returnData });
+        res.status(200).json({ classData: returnData, formArray: formArray });
     } catch (error) {
-
+        console.log(error);
     }
 });
 
