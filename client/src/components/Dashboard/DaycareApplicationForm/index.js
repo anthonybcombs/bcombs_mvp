7f74f823e4dfcbe9fc423e8561105d29caec3c69
dennis <sanjosedennis7593@gmail.com>
2021-09-08 17:44:42 +0800

    fixes on daycare

--- client/src/components/Dashboard/DaycareApplicationForm/index.js
+++ client/src/components/Dashboard/DaycareApplicationForm/index.js
@@ -155,6 +155,7 @@ export default function index() {
   );
 
   const DATE_TIME_FORMAT = "MM/dd/yyyy hh:mm:ss";
+  const SAVE_DATE_FORMAT = "yyyy-MM-dd";
 
   const { vendor_id } = useParams();
 
@@ -525,7 +526,7 @@ export default function index() {
         zip_code: parent.profile.zip_code !== '' ? parent.profile.zip_code : childProfile.zip_code,
         birthdate: format(
           new Date(parent.profile.date_of_birth),
-          DATE_TIME_FORMAT),
+          SAVE_DATE_FORMAT),
         gender: parent.profile.gender,
         age: getAge(parent.profile.date_of_birth),
         ethnicities: getAppEtnicities(parent.profile.ethinicity),
@@ -571,7 +572,7 @@ export default function index() {
           age: getAge(childsInformation[i].profile.date_of_birth),
           birthdate: format(
             new Date(childsInformation[i].profile.date_of_birth),
-            DATE_TIME_FORMAT),
+            SAVE_DATE_FORMAT),
           gender: childsInformation[i].profile.gender,
           address: childsInformation[i].profile.address,
           city: childsInformation[i].profile.city,
@@ -580,7 +581,7 @@ export default function index() {
           child_lives_with: getChildLivesWith(childsInformation[i].profile.child_lives_with),
           preffered_start_date:format(
             new Date(childsInformation[i].profile.preffered_start_date),
-            DATE_TIME_FORMAT),
+            SAVE_DATE_FORMAT),
           current_classroom: childsInformation[i].profile.current_classroom,
           primary_language: childsInformation[i].profile.primary_language,
           needed_days: childsInformation[i].profile.needed_days,
@@ -615,11 +616,11 @@ export default function index() {
         },
         parents: setupParentsList( childsInformation[i].profile),
         section1_signature: termsWaiver.section1.signature,
-        section1_date_signed: format(new Date(termsWaiver.date), DATE_TIME_FORMAT),
+        section1_date_signed: format(new Date(termsWaiver.date), SAVE_DATE_FORMAT),
         section2_signature: termsWaiver.section2.signature,
-        section2_date_signed: format(new Date(termsWaiver.date), DATE_TIME_FORMAT),
+        section2_date_signed: format(new Date(termsWaiver.date), SAVE_DATE_FORMAT),
         section3_signature: termsWaiver.section3.signature,
-        section3_date_signed: format(new Date(termsWaiver.date), DATE_TIME_FORMAT),
+        section3_date_signed: format(new Date(termsWaiver.date), SAVE_DATE_FORMAT),
         section1_text: vendor.section1_text,
         section2_text: vendor.section2_text,
         section3_text: vendor.section3_text,
@@ -639,7 +640,7 @@ export default function index() {
       is_daycare: true
     };
 
-
+    console.log('PAYLOADDDDDD', payload)
     dispatch(requestAddApplication(payload));
   }
 
