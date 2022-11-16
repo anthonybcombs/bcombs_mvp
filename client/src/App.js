c4314d6978f896713a1b7e256f9bbb3bb8e12934
dennis <sanjosedennis7593@gmail.com>
2022-11-16 12:24:17 +0800

    update landing page

--- client/src/App.js
+++ client/src/App.js
@@ -168,7 +168,14 @@ const AsyncDisplayCalendar = Loadable({
 });
 
 const AsyncLandingPage = Loadable({
-  loader: () => import('./components/LandingPage'),
+  loader: () => {
+
+    if(window.innerWidth <= 768) {
+      return import('./components/LandingPage/Pages/mobile/home/index.js');
+    }
+    
+    return import('./components/LandingPage/Pages/desktop/home/index.js');
+  },
   loading: Loading
 });
 
